7989c95478b81048cfe6cb1b3a496aee3c9e9141
Nikhil <kumarnikhil48578@gmail.com>
2025-04-04 02:56:12 +0530

    fix cursor

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,6 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
-import CustomCursor from './components/CustomCursor/CustomCursor.jsx';
 import Layout from "./Layout.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
 import NavbarComForUser from "./pages/NavbarPage.jsx";
@@ -16,42 +15,17 @@ import BentoPage from "./pages/BentoPage.jsx";
 import FooterPage from "./pages/FooterPage.jsx"
 import CardPage from "./pages/CardPage.jsx";
 
-const CursorWrapper = ({ children }) => (
-  <>
-    <CustomCursor />
-    {children}
-  </>
-);
 
-const ScrollWrapper = ({ children }) => {
-  useEffect(() => {
 
-    const scrollContainers = document.querySelectorAll('[data-scroll-container]')
-    scrollContainers.forEach(container => {
-      container.removeAttribute('data-scroll-container')
-    })
-    
-    const mainElement = document.querySelector('main')
-    if (mainElement) {
-      mainElement.setAttribute('data-scroll-container', '')
-    }
-  }, [])
-
-  return children
-}
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path='/' element={
-        <CursorWrapper>
-          <ScrollWrapper>
             <LandingPage/>
-          </ScrollWrapper>
-        </CursorWrapper>
       } />
 
-      <Route path="/dashboard" element={<CursorWrapper><Layout /></CursorWrapper>}>
+      <Route path="/dashboard" element={<Layout />}>
         <Route path="navbar" element={<NavbarComForUser />} />
         <Route path="bento" element={<BentoPage/>} />
         <Route path="footer" element={<FooterPage/>} />
