709fba8e648f27d34606a1250cc6baa12e0ae060
Nikhil <kumarnikhil48578@gmail.com>
2025-04-07 18:41:39 +0530

    remove locomotive

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./components/Sidebar/Sidebar";
-import useLocomotiveScroll from "./hooks/useLocomotiveScroll";
+
 
 function Layout() {
-  useLocomotiveScroll();
   return (
     <div className="min-h-screen bg-white">
       <Sidebar />
-      <main className="flex-1 p-4 md:p-6" data-scroll-container>
+      <main className="flex-1 p-4 md:p-6" >
         <Outlet />
       </main>
     </div>
