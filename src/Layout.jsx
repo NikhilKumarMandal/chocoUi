4160f1f662192340ce972d018c6ddafbe7789fbe
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-04-09 17:17:15 +0530

    Fix Layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,23 +4,17 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import Navbar from "./components/Navbar/Navbar";
 
 function Layout() {
-
-
-function Layout() {
-
   return (
     <div className="min-h-screen bg-white flex">
       <Navbar />
       <Sidebar />
 
       <main className="flex-1 p-4 mt-20 md:p-6 flex items-center justify-center">
-
-      <main className="flex-1 p-4 md:p-6" >
-
+        <main className="flex-1 p-4 md:p-6"></main>
         <Outlet />
       </main>
     </div>
-  )
+  );
 }
 
 export default Layout;
