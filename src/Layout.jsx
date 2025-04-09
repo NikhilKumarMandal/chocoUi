8271132d7ea70e0b1c3ac746acdc4868da5ff88f
Nikhil <kumarnikhil48578@gmail.com>
2025-04-09 16:52:42 +0530

    fix bugs

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,7 +3,6 @@ import Sidebar from "./components/Sidebar/Sidebar";
 
 import Navbar from "./components/Navbar/Navbar";
 
-function Layout() {
 
 
 function Layout() {
@@ -16,11 +15,11 @@ function Layout() {
       <main className="flex-1 p-4 mt-20 md:p-6 flex items-center justify-center">
 
       <main className="flex-1 p-4 md:p-6" >
-
+        </main>
         <Outlet />
       </main>
     </div>
-  );
+  )
 }
 
 export default Layout;
